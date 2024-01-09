d14252d8dbf67092ac52eb8f8c19244eb9f986e5
Andrew Thomas <andrew@andrewrthomas.com>
2024-01-09 11:21:34 -0800

    ok ok ok

--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,8 @@ const assert = std.debug.assert;
 const print = std.debug.print;
 
 const complex = std.math.Complex(f32);
+const cos = std.math.cos;
+const sin = std.math.sin;
 
 fn fft(comptime size: usize, signal: [size]complex) [size]complex {
     // make sure that we have a power of two
@@ -29,11 +31,33 @@ fn fft(comptime size: usize, signal: [size]complex) [size]complex {
         }
     }
 
+    // then we do the movie magic
+    // TODO: lots of type conversion
+    const n: f32 = @floatFromInt(size);
+    for (1..@log2(n)) |i| {
+        const idx: f32 = @floatFromInt(i);
+        const m = std.math.pow(f32, 2, idx);
+        const x = (-2 * std.math.pi) / m;
+        const twiddle = complex{ .re = cos(x), .im = sin(x) };
+
+        var k: usize = 1;
+        while (k < size) : (k += m) {
+            var w: u16 = 1;
+            for (0..((m / 2) - 1)) |j| {
+                const temp = w * mutable_signal[k + j + (m / 2)];
+                const uemp = mutable_signal[k + j];
+                mutable_signal[k + j] = temp.add(uemp);
+                mutable_signal[k + j + (m / 2)] = uemp.sub(temp);
+                w = w * twiddle;
+            }
+        }
+    }
+
     return signal;
 }
 
 test "power of two check" {
-    const signal: [8]complex = .{ complex{ .im = 0, .re = 1.0 }, complex{ .im = 0, .re = 2.0 }, complex{ .im = 0, .re = 3.0 }, complex{ .im = 0, .re = 4.0 }, complex{ .im = 0, .re = 5.0 }, complex{ .im = 0, .re = 6.0 }, complex{ .im = 0, .re = 7.0 }, complex{ .im = 0, .re = 8.0 } };
+    const signal: [8]complex = .{ complex{ .im = 0, .re = 0.0 }, complex{ .im = 0, .re = 1.0 }, complex{ .im = 0, .re = 2.0 }, complex{ .im = 0, .re = 3.0 }, complex{ .im = 0, .re = 4.0 }, complex{ .im = 0, .re = 5.0 }, complex{ .im = 0, .re = 6.0 }, complex{ .im = 0, .re = 7.0 } };
     const result = fft(8, signal);
     print("{any}", .{result});
 }
