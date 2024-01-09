b5ca3bc9ee607adc4ce6b6f58fa548712d0f0256
Andrew Thomas <andrew@andrewrthomas.com>
2024-01-08 16:49:45 -0800

    ok so the problem is that we are reversing all the way to the end, only want bits to be up to max size

--- src/main.zig
+++ src/main.zig
@@ -5,25 +5,35 @@ const print = std.debug.print;
 const complex = std.math.Complex(f32);
 
 fn fft(comptime size: usize, signal: [size]complex) [size]complex {
+    // make sure that we have a power of two
+    // and that the indecies can be represented as u16 for bit reversal
+    assert(size % 2 == 0);
+    assert(size <= 65535);
+
     var out: [size]complex = undefined;
-    const log2 = @log2(@as(f32, size));
+    _ = out;
+
+    var mutable_signal = signal;
 
     // first we do a bit reversal
-    for (signal, 0..) |val, i| {
+    for (mutable_signal, 0..) |val, i| {
         _ = val;
-        for (0..log2) |j| {
-            const match = ((1 << j) & i) == 0;
-            _ = match;
-            var new = undefined;
-            new |= (1 << log2 - 1 - j);
-        }
+        const index: u16 = @intCast(i);
+        const reversed: u16 = @bitReverse(index);
+        print("index: {d}\n", .{index});
+        print("reversed: {d}\n", .{reversed});
+
+        // if (i <= reversed) {
+        //mutable_signal[i] = mutable_signal[reversed];
+        //mutable_signal[reversed] = val;
+        //}
     }
 
-    return out;
+    return signal;
 }
 
 test "power of two check" {
-    const signal: [4]complex = .{ complex{ .im = 0, .re = 1.0 }, complex{ .im = 0, .re = 1.0 }, complex{ .im = 0, .re = 1.0 }, complex{ .im = 0, .re = 1.0 } };
+    const signal: [4]complex = .{ complex{ .im = 0, .re = 1.0 }, complex{ .im = 0, .re = 2.0 }, complex{ .im = 0, .re = 3.0 }, complex{ .im = 0, .re = 4.0 } };
     const result = fft(4, signal);
     print("{any}", .{result});
 }
