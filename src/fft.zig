502829ab8db915adc2cdf05b0758ecfbc9e59308
Andrew Thomas <andrew@andrewrthomas.com>
2024-02-09 13:17:32 -0800

    pointer stuff

--- src/fft.zig
+++ src/fft.zig
@@ -7,11 +7,6 @@ pub fn FFT(comptime size: usize) !type {
     // make sure that we have a power of two
     if (size % 2 != 0) @compileError("input size must be a power of two");
 
-    const FFTData = struct {
-        reals: *const [size]f32,
-        imaginaries: *const [size]f32,
-    };
-
     return struct {
         const log: f32 = @log2(@as(f32, @floatFromInt(size)));
         const bit_rev: @Vector(size, i32) = build_bit_rev(size);
@@ -22,7 +17,9 @@ pub fn FFT(comptime size: usize) !type {
                 ims: @Vector(log, f32),
             },
         );
-        pub fn real_to_complex(signal: *const [size]f32) FFTData {
+        pub fn real_to_complex(signal: *const [size]f32, real_out: *[size]f32, im_out: *[size]f32) void {
+            var r_out = real_out;
+            var i_out = im_out;
             var real_vec: @Vector(size, f32) = signal.*;
             var im_vec: @Vector(size, f32) = [_]f32{0.0} ** size;
 
@@ -56,10 +53,9 @@ pub fn FFT(comptime size: usize) !type {
                 }
             }
 
-            return FFTData{
-                .reals = &real_vec,
-                .imaginaries = &im_vec,
-            };
+            // TODO figure out pointer situation
+            r_out = @as([size]f32, real_vec);
+            i_out = @as([size]f32, im_vec);
         }
     };
 }
@@ -106,12 +102,12 @@ fn build_bit_rev(comptime size: usize) @Vector(size, i32) {
 test "simple 8 point test" {
     const signal: [8]f32 = .{ 0, 1, 2, 3, 4, 5, 6, 7 };
     const f = try FFT(8);
-    const out = f.real_to_complex(&signal);
-    const reals = out.reals;
-    const ims = out.imaginaries;
+    var real_out: [8]f32 = undefined;
+    var im_out: [8]f32 = undefined;
+    _ = f.real_to_complex(&signal, &real_out, &im_out);
 
-    try testing.expect(std.mem.eql(f32, reals, &[8]f32{ 28, -3.999999523162842, -4, -4, -4, -4, -4, -4 }));
-    try testing.expect(std.mem.eql(f32, ims, &[8]f32{ 0, 9.656853675842285, 3.999999761581421, 1.6568536758422852, 0, -1.6568536758422852, -3.999999761581421, -9.656853675842285 }));
+    try testing.expect(std.mem.eql(f32, &real_out, &[8]f32{ 28, -3.999999523162842, -4, -4, -4, -4, -4, -4 }));
+    try testing.expect(std.mem.eql(f32, &im_out, &[8]f32{ 0, 9.656853675842285, 3.999999761581421, 1.6568536758422852, 0, -1.6568536758422852, -3.999999761581421, -9.656853675842285 }));
 }
 
 test "real world test" {
